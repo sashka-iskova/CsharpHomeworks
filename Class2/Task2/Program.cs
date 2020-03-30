54b6e86079926400c913702a29f47ec43ea2b789
sashka-iskova <58604033+sashka-iskova@users.noreply.github.com>
2020-03-30 19:00:58 +0200

    Add files via upload

---
+++ Class2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class02_Homework_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Average Number");
+            Console.WriteLine("Enter the First Number");
+            string firstInput = Console.ReadLine();
+            int firstNumber = Int32.Parse(firstInput);
+            Console.WriteLine("Enter the Second Number");
+            string secondInput = Console.ReadLine();
+            int secondNumber = Int32.Parse(secondInput);
+            Console.WriteLine("Enter the Third Number");
+            string thirdInput = Console.ReadLine();
+            int thirdNumber = Int32.Parse(thirdInput);
+            Console.WriteLine("Enter the Forth Number");
+            string forthInput = Console.ReadLine();
+            int forthNumber = Int32.Parse(forthInput);
+            Console.WriteLine((firstNumber + secondNumber + thirdNumber + forthNumber) / 4);
+
+        }
+    }
+}
