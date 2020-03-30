60e72a48f405f0032cda8605043456626ff0e543
sashka-iskova <58604033+sashka-iskova@users.noreply.github.com>
2020-03-31 00:21:41 +0200

    Update Program.cs

--- Class2/Task1/Program.cs
+++ Class2/Task1/Program.cs
@@ -6,15 +6,13 @@ namespace Class02_Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Real Calculator");
+           Console.WriteLine("Real Calculator");
             Console.WriteLine("Please enter first number");
-            string firstInput = Console.ReadLine();
-            int firstInteger = Int32.Parse(firstInput);
+            int firstInteger = Int32.Parse(Console.ReadLine());
             Console.WriteLine("What operation would you like to be done?");
             string operation = Console.ReadLine();
             Console.WriteLine("Please enter second number");
-            string secondInput = Console.ReadLine();
-            int secondInteger = Int32.Parse(secondInput);
+            int secondInteger = Int32.Parse(Console.ReadLine());
             if (operation == "+")
             {
                 Console.WriteLine(firstInteger + secondInteger);
