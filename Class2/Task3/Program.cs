0dac75b8d0d610e6134d011dff44e6dd63732fbd
sashka-iskova <58604033+sashka-iskova@users.noreply.github.com>
2020-03-30 22:34:39 +0200

    Add files via upload

---
+++ Class2/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class02_Homework_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Swap Numbers");
+            Console.WriteLine("Input the first number");
+            string firstInput = Console.ReadLine();
+            int x = Int32.Parse(firstInput);
+            Console.WriteLine("Input the second number");
+            string secondInput = Console.ReadLine();
+            int y = Int32.Parse(secondInput);
+            x = x + y; 
+            y = x - y; 
+            x = x - y;
+            Console.WriteLine("Output after swaping");
+            Console.WriteLine("The value for x is: " + x);
+            Console.WriteLine("The value for y is: " + y);
+
+        }
+    }
+}
