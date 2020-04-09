43f3c01bce89f7001deb454ce04dbe53b41e7d6b
sashka-iskova <sashkaiskova@gmail.com>
2020-04-09 14:05:56 +0200

    Class5 first task

---
+++ Class5/Class5-Task1-Solution/Model/Dog.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Model
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string name, string race, string color)
+        {
+            Name = name;
+            Race = race;
+            Color = color;
+        }
+        public string eat()
+        {
+            return "is now eating.";
+        }
+        public string play()
+        {
+            return "is now playing.";
+        }
+        public string ChaseTail()
+        {
+            return "is now chasing its tail.";
+        }
+      
+    }
+}
---
+++ Class5/Class5-Task1-Solution/Task1-Project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Model;
+
+namespace Task1_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your dogs name");
+            string dogName = Console.ReadLine();
+            Console.WriteLine("Please enter your dogs race");
+            string dogRace = Console.ReadLine();
+            Console.WriteLine("Please enter your dogs color");
+            string dogColor = Console.ReadLine();
+            Dog dog1 = new Dog(dogName, dogRace, dogColor);
+            Console.WriteLine("What would you like your dog to do?\nPlease select a number:");
+            Console.WriteLine("1. Eat");
+            Console.WriteLine("2. Play");
+            Console.WriteLine("3. Chase its tail");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.WriteLine($"Your dogs name is {dog1.Name} its race is {dog1.Race} and {dog1.eat()}");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine($"Your dogs name is {dog1.Name} its race is {dog1.Race} and {dog1.play()}");
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine($"Your dogs name is {dog1.Name} its race is {dog1.Race} and {dog1.ChaseTail()}");
+            }
+        }
+
+    }
+}
